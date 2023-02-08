e796d9db576f0110a78bbeeb85f996a42c422a81
pol santos <pol.santos@enti.cat>
2023-02-08 13:39:55 +0100

    mas cosas

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 const http = require("http");
 const fs = require("fs");
 
-http.createServer(function(request, response){
-	
+function send_index (response)
+{
+
 	fs.readFile("index.html", function(err, data){
 		if (err){
 			console.error(err);
@@ -18,5 +19,44 @@ http.createServer(function(request, response){
 		response.end();
 	});
 
+
+}
+
+
+
+function send_player (response)
+{
+	fs.readFile("index.html", function(err, data){
+		if (err){
+			console.error(err);
+			return;
+		}
+
+
+		response.writeHead(200, {"content-Type":"text/html"});
+		response.write(data);
+
+		response.end();
+	});
+
+
+}
+
+
+http.createServer(function(request, response){
+
+
+
+	let url = request.url.split("/");
+
+	switch (url[1]){
+		case "player.png":
+			send_player(response);
+		break;
+
+		default:
+			send_index(response);
+		
+	}
 
 }).listen(6969);
