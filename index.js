0e4babb546e9ca622f33c99be5da17a60dc6a67b
pol santos <pol.santos@enti.cat>
2023-02-10 14:33:40 +0100

    se mueven los bichos

--- index.js
+++ index.js
@@ -1,60 +1,51 @@
+#!/usr/bin/env node
 
+const http = require('http');
+const fs = require('fs');
 
-const http = require("http");
-const fs = require("fs");
+function sendIndex (response) {
+	
+	fs.readFile("index.html", function (err, data) {
 
-function send_index (response)
-{
-
-	fs.readFile("index.html", function(err, data){
-		if (err){
+		if (err) {
 			console.error(err);
 			return;
 		}
 
-
-		response.writeHead(200, {"content-Type":"text/html"});
+		response.writeHead(200, {"Content-Type": "text/html"});
 		response.write(data);
-
 		response.end();
+
 	});
-
-
 }
 
+function sendPlayer (response) {
+	
+	fs.readFile("player.png", function (err, data) {
 
-
-function send_player (response)
-{
-	fs.readFile("index.html", function(err, data){
-		if (err){
+		if (err) {
 			console.error(err);
 			return;
 		}
 
-
-		response.writeHead(200, {"content-Type":"text/html"});
+		response.writeHead(200, {"Content-Type": "image/png"});
 		response.write(data);
-
 		response.end();
 	});
-
-
 }
 
-
-http.createServer(function(request, response){
-	console.log(request.url);
+http.createServer(function (request, response) {
+	
 	let url = request.url.split("/");
 
-	switch (url[1]){
-		case "player.png":
-			send_player(response);
+	switch(url[1]) {
+	
+	case "player.png":
+		sendPlayer(response);
 		break;
 
-		default:
-			send_index(response);
-		
+	default:
+		sendIndex(response);
 	}
 
 }).listen(6969);
